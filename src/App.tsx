d90fd015fda4e6092a33f7e0999acfe1b13954f0
Malik Hamza <malikhamza.v@gmail.com>
2023-01-08 19:53:26 +0500

    complete gym landing page

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <div style={{textAlign:"center", backgroundColor:"black", color:"white"}}>Please note that the current version hosted on the production link is merely a frontend prototype and does not reflect the entirety of the project.
+</div>
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
